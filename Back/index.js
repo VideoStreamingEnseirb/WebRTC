3e910c6fe6130a9b08cd55dfbc7ae5aee2586d18
Nathauy <uy.nathalie@yahoo.com>
2019-12-15 22:50:25 +0100

    Peer connection between same navigator

---
+++ Back/index.js
@@ -0,0 +1,59 @@
+const static = require('node-static');
+const http = require('http');
+const file = new(static.Server)();
+
+const app = http.createServer(function(req, res) {
+  file.server(req, res);
+}).listen(2013);
+
+
+const io = require('socket.io').listen(app);
+
+
+io.sockets.on('connection', (socket) => {
+
+  function log() {
+    const array = ['>>>> Message from server:'];
+    for (const i = 0; i< arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+    socket.emit('log', array);
+
+  }
+
+  socket.on('message', (message) => {
+    log('Got message:', message);
+    socket.broadcast.emit('message', message);
+  });
+
+
+  socket.on('create or join', (room) => {
+    const numClients = io.sockets.clients(room).length;
+     
+    log ('Room' + room + 'has' + numClients + 'client(s)');
+    log('Request to create or join room' + room);
+
+    if (numClients === 0 ) {
+      socket.join(room);
+      socket.emit('created', room);
+    } else if (numClients === 1) {
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joinded', room);
+    } else {
+      socket.emit('full', room)
+    }
+
+    socket.emit('emit() : client' + socket.id + 'joinded room' + room);
+    socket.broadcast.emit('broadcast(): client' + socket.id + 'joinded room' + room);
+
+  })
+
+
+
+
+
+
+
+
+});
--- Front/src/components/demo.js
+++ Front/src/components/demo.js
@@ -1,32 +1,201 @@
 import React, { Component } from 'react'
 
-import ReactDOM from "react-dom";
 
-
-
-const constraints = {
-    video : true,
-    audio : true
+//Logs actions 
+function trace(text) {
+    text = text.trim();
+    const now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now, text);
 }
 
 
 
 export class demo extends Component {
 
-    componentDidMount() {
-        const node = ReactDOM.findDOMNode(this);
+    state = {
+        startDisabled: false,
+        callDisabled: true,
+        hangUpDisabled: true,
+        servers: null,
+        pc1: null,
+        pc2: null,
+        localStream: null
+    };
 
-        if (node instanceof HTMLElement) {
-            var video = node.querySelector('video');
+    localVideoRef = React.createRef();
+    remoteVideoRef = React.createRef();
+
+    start = () => {
+        this.setState({
+            startDisabled: true
+        });
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: true,
+                video: true
+            })
+            .then(this.gotStream)
+            .catch(e => alert("getUserMedia() error:" + e.name));
+    };
+    gotStream = stream => {
+        this.localVideoRef.current.srcObject = stream;
+        this.setState({
+            callDisabled: false,
+            localStream: stream
+        });
+    };
+    gotRemoteStream = event => {
+        let remoteVideo = this.remoteVideoRef.current;
+
+        if (remoteVideo.srcObject !== event.streams[0]) {
+            remoteVideo.srcObject = event.streams[0];
         }
-        console.log(node)
-        console.log(video)
-        navigator.mediaDevices.getUserMedia(constraints).
-        then((stream) => {
-            video.srcObject = stream
-    
-        })
-    }
+    };
+
+    call = () => {
+        this.setState({
+            callDisabled: true,
+            hangUpDisabled: false
+        });
+        let { localStream } = this.state;
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/RTCIceServer
+        let servers = null,
+            pc1 = new RTCPeerConnection(servers),
+            pc2 = new RTCPeerConnection(servers);
+
+        pc1.onicecandidate = e => this.onIceCandidate(pc1, e);
+        pc1.oniceconnectionstatechange = e => this.onIceStateChange(pc1, e);
+
+        pc2.onicecandidate = e => this.onIceCandidate(pc2, e);
+        pc2.oniceconnectionstatechange = e => this.onIceStateChange(pc2, e);
+        pc2.ontrack = this.gotRemoteStream;
+
+        localStream
+            .getTracks()
+            .forEach(track => pc1.addTrack(track, localStream));
+
+        pc1
+            .createOffer({
+                offerToReceiveAudio: 1,
+                offerToReceiveVideo: 1
+            })
+            .then(this.onCreateOfferSuccess, error =>
+                console.error(
+                    "Failed to create session description",
+                    error.toString()
+                )
+            );
+
+        console.log("servers after call", servers);
+
+        this.setState({
+            servers,
+            pc1,
+            pc2,
+            localStream
+        });
+    };
+
+    onCreateOfferSuccess = desc => {
+        let { pc1, pc2 } = this.state;
+
+        pc1
+            .setLocalDescription(desc)
+            .then(
+                () =>
+                    console.log("pc1 setLocalDescription complete createOffer"),
+                error =>
+                    console.error(
+                        "pc1 Failed to set session description in createOffer",
+                        error.toString()
+                    )
+            );
+
+        pc2.setRemoteDescription(desc).then(
+            () => {
+                console.log("pc2 setRemoteDescription complete createOffer");
+                pc2
+                    .createAnswer()
+                    .then(this.onCreateAnswerSuccess, error =>
+                        console.error(
+                            "pc2 Failed to set session description in createAnswer",
+                            error.toString()
+                        )
+                    );
+            },
+            error =>
+                console.error(
+                    "pc2 Failed to set session description in createOffer",
+                    error.toString()
+                )
+        );
+    };
+
+    onCreateAnswerSuccess = desc => {
+        let { pc1, pc2 } = this.state;
+
+        pc1.setRemoteDescription(desc).then(
+            () => {
+                console.log("pc1 setRemoteDescription complete createAnswer");
+                console.log("servers after createAnswer", this.state.servers);
+            },
+            error =>
+                console.error(
+                    "pc1 Failed to set session description in onCreateAnswer",
+                    error.toString()
+                )
+        );
+
+        pc2
+            .setLocalDescription(desc)
+            .then(
+                () =>
+                    console.log(
+                        "pc2 setLocalDescription complete createAnswer"
+                    ),
+                error =>
+                    console.error(
+                        "pc2 Failed to set session description in onCreateAnswer",
+                        error.toString()
+                    )
+            );
+    };
+
+    onIceCandidate = (pc, event) => {
+        let { pc1, pc2 } = this.state;
+
+        let otherPc = pc === pc1 ? pc2 : pc1;
+
+        otherPc
+            .addIceCandidate(event.candidate)
+            .then(
+                () => console.log("addIceCandidate success"),
+                error =>
+                    console.error(
+                        "failed to add ICE Candidate",
+                        error.toString()
+                    )
+            );
+    };
+
+    onIceStateChange = (pc, event) => {
+        console.log("ICE state:", pc.iceConnectionState);
+    };
+
+    hangUp = () => {
+        let { pc1, pc2 } = this.state;
+
+        pc1.close();
+        pc2.close();
+
+        this.setState({
+            pc1: null,
+            pc2: null,
+            hangUpDisabled: true,
+            callDisabled: false
+        });
+    };
 
     render() {
         return (
@@ -36,8 +205,37 @@ export class demo extends Component {
             </div>
                 <div className="card-body">
                     <p className="card-text">Bon courage pour cette partie.</p>
-                    <div id="container-fluid center">
-                        <video autoPlay></video>
+                    <div className="container-fluid center">
+                        <div>
+                            <video
+                                ref={this.localVideoRef}
+                                autoPlay
+                                muted
+                                style={{
+                                    width: "240px",
+                                    height: "180px"
+                                }}
+                            />{" "}
+                            <video
+                                ref={this.remoteVideoRef}
+                                autoPlay
+                                style={{
+                                    width: "240px",
+                                    height: "180px"
+                                }}
+                            />
+                            <div>
+                                <button onClick={this.start} disabled={this.startDisabled}>
+                                    Start{" "}
+                                </button>{" "}
+                                <button onClick={this.call} disabled={this.callDisabled}>
+                                    Call{" "}
+                                </button>{" "}
+                                <button onClick={this.hangUp} disabled={this.hangUpDisabled}>
+                                    Hang Up{" "}
+                                </button>{" "}
+                            </div>{" "}
+                        </div>
                     </div>
                 </div>
             </div>
