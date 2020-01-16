041efa1617baf1c42b5c3b078da2498e7aad461a
mlehanneur <mlehanneur@enseirb-matmeca.fr>
2020-01-16 03:17:38 -0800

    links

--- Front/src/components/biblio.js
+++ Front/src/components/biblio.js
@@ -16,10 +16,12 @@ export class Biblio extends Component {
                   In this section, are listed links which have been used to understant WebRTC protocol and to redact this web page.
               </p>
               <ul>Bibliography
-                  <li style={{ "text-indent": "50px" }}> The link to the official WebRTC page for explainations:  <br />  <a href="https://webrtc.org/start/"> https://webrtc.org/start/ </a></li>
-                  <li style={{ "text-indent": "50px" }}> This first page allow us to understant the WebRTC project in his globality: <br /><a href="https://en.wikipedia.org/wiki/WebRTC"> https://en.wikipedia.org/wiki/WebRTC </a>  </li>
+                  <li style={{ "text-indent": "50px" }}> The link to the official WebRTC page for explanations:  <br />  <a href="https://webrtc.org/start/"> https://webrtc.org/start/ </a></li>
+                  <li style={{ "text-indent": "50px" }}> This page allows us to understand the WebRTC project in his globality: <br /><a href="https://en.wikipedia.org/wiki/WebRTC"> https://en.wikipedia.org/wiki/WebRTC </a>  </li>
                   <li style={{ "text-indent": "50px" }}> This site was usefull to create our web application: <br />  <a href="https://codeutopia.net/blog/2016/02/08/using-webrtc-and-react-to-build-a-basic-chat-server/"> https://codeutopia.net/blog/2016/02/08/using-webrtc-and-react-to-build-a-basic-chat-server/ </a></li>
                   <li style={{ "text-indent": "50px" }}> Usefull tips to implement WebRTC:  <br />  <a href="https://www.html5rocks.com/en/tutorials/webrtc/basics/"> https://www.html5rocks.com/en/tutorials/webrtc/basics/ </a></li>
+                  <li style={{ "text-indent": "50px" }}> Website that justifies the creation of WebRTC:  <br />  <a href="https://bloggeek.me/massive-applications-using-webrtc/"> https://bloggeek.me/massive-applications-using-webrtc/ </a></li>
+
               </ul>
             </div>
           </div>
