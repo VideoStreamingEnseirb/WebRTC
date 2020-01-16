c070e7479d335a153d0e5be6fbc88aa0bef5a8da
Kejji <akejji@enseirb-matmeca.fr>
2020-01-16 11:58:39 +0100

    avancement logs

--- Front/src/components/logs.js
+++ Front/src/components/logs.js
@@ -2,17 +2,50 @@ import React from "react";
 import "webrtc-adapter";
 import faker from "faker";
 class Logs extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+    dateOf = (line)=> {
+        var liste = line.split(']');
+        var date = "<b>"+ liste[0]+']'+"</b>";
+        var a ;
+        a=String(liste[1]).substring(1,17);
+        if(a.localeCompare("Connection close")==0){
+            liste[1]='<font color="red">'+liste[1]+'</font>';
+        }
+        if(a.localeCompare("Received Message")==0){
+            liste[1]='<font color="green">'+liste[1]+'</font>';
+        }
+        if(a.localeCompare("Server is listen")==0){
+            liste[1]='<font color="magenta">'+liste[1]+'</font>';
+        }
+        if(a.localeCompare("Connection accep")==0){
+            liste[1]='<font color="blue">'+liste[1]+'</font>';
+        }
+        var messageTotale=date+liste[1];
+        return messageTotale;
+  };
     componentDidMount() {
         window.setInterval(function(){
-            fetch('http://127.0.0.1:4000',{method:"get"} ).then((function(response) 
+            fetch('http://127.0.0.1:4000',{method:"get"} ).then( (response) =>
             {
-                response.text().then(function(text)      
+                response.text().then((text)=>     
                 {
-                    document.querySelector("#klog").innerHTML=text}
-                );
-        })
+                    var lines=text.split('\n');
+                    return lines;
+                }
+                ).then((lines)=>{
+                    var i=0;
+                    for(i=0;i<lines.length;i++){
+                        lines[i] = this.dateOf(lines[i]);
+                        //console.log(lines[i]);
+                        //document.querySelector("#klog").innerHTML+=t;
+                    }
+                    document.querySelector("#klog").innerHTML=lines;
+                });
+        }
         );
-        }, 2000);
+        }.bind(this), 2000);
     };
     render() {
         return (
@@ -25,3 +58,7 @@ class Logs extends React.Component {
     }
 }
 export default Logs
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
