fd78d3a84ff0d46346f0dd85d7e333692cc6b1fd
Nathauy <nuy@enseirb-matmeca.fr>
2020-01-21 22:22:07 +0100

    Change image

--- Front/src/components/biblio.js
+++ Front/src/components/biblio.js
@@ -14,14 +14,14 @@ export class Biblio extends Component {
         <div className="card-body">
           <div className="card-text ml-3 mr-3">
               <p>
-              In this section, are listed links which have been used to understant WebRTC protocol and to redact this web page.
+              In this section, are listed links which have been used to understand WebRTC protocol and for the redaction of this web page.
               </p>
 
           </div>
 
           <ul>
             <li> The link to the official WebRTC page for explanations:  <br />  <a href="https://webrtc.org/start/"> https://webrtc.org/start/ </a></li>
-            <li> This page allows us to understand the WebRTC project in his globality: <br /><a href="https://en.wikipedia.org/wiki/WebRTC"> https://en.wikipedia.org/wiki/WebRTC </a>  </li>
+            <li> This page allowed us to understand the WebRTC project in his globality: <br /><a href="https://en.wikipedia.org/wiki/WebRTC"> https://en.wikipedia.org/wiki/WebRTC </a>  </li>
             <li> This site was usefull to create our web application: <br />  <a href="https://codeutopia.net/blog/2016/02/08/using-webrtc-and-react-to-build-a-basic-chat-server/"> https://codeutopia.net/blog/2016/02/08/using-webrtc-and-react-to-build-a-basic-chat-server/ </a></li>
             <li> Usefull tips to implement WebRTC:  <br />  <a href="https://www.html5rocks.com/en/tutorials/webrtc/basics/"> https://www.html5rocks.com/en/tutorials/webrtc/basics/ </a></li>
             <li> Website that justifies the creation of WebRTC:  <br />  <a href="https://bloggeek.me/massive-applications-using-webrtc/"> https://bloggeek.me/massive-applications-using-webrtc/ </a></li>
--- Front/src/components/presentation.js
+++ Front/src/components/presentation.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
 
-
 import InitiationPhase from '../Images/InitiationPhase.png'
 import PeerCommunication from '../Images/PeerCommunication.png'
 
 import MediaProtocolStack from '../Images/MediaProtocolStack.png'
 import SignallingProtocolStack from '../Images/SignallingProtocolStack.png'
 
+
+
+
+
 export class presentation extends Component {
+
+
     render() {
+
+
         return (
-
-
-
-
             <div className="card">
                 <div className="card-header">
-                    WebRTC
-                        </div>
+                    WebRTC <em>(Web Real-Time Communication)</em>
+                </div>
+
 
                 <div className="card-body">
 
@@ -32,10 +36,10 @@ export class presentation extends Component {
                         With the creation of rich web pages, the development of animations and the apparition of new functionality like drag
                          and drop system to share files on websites, the development of <strong>real-time data</strong> sharing and bidirectional <strong>
                             video flux </strong> lead
-                    to the creation of a fast and almost real-time protocol.
-                     Direct exchanges between navigators already existed thanks to the creation of different plugins like Adobe Flash or Microsoft Active X.
+to the creation of a fast and almost real-time protocol.
+Direct exchanges between navigators already existed thanks to the creation of different plugins like Adobe Flash or Microsoft Active X.
                          Yet, these proprietary solutions weren’t <strong>standardized</strong> and thus, because of the diversity, not really useful.
-                         To overcome this lack of coherence <strong>WebRTC</strong> has been created by Justin Uberti and Peter Thatcher.
+                         To overcome this lack of coherence <strong>WebRTC</strong> <em>(Web Real-Time Communication)</em> has been created by Justin Uberti and Peter Thatcher.
                               Source code of Web RTC can be found in the following link : {"\n"}
                         <a href={"https://webrtc.googlesource.com/src"}>https://webrtc.googlesource.com/src </a>
                     </p>
@@ -48,7 +52,7 @@ export class presentation extends Component {
                         </h4>
                     <hr></hr>
 
-                    <p> <strong>WebRTC</strong> <em>(Web Real-Time Communication)</em> main purpose is to provide <strong>real-time communication</strong>, it is an API that has three main services and is based on a Javascript API. It involves two different entities : <em>real-time communication</em> and <em>server</em>. The server is here to initialize the connection between the two peers before they communicate with each other directly by sharing data, audio and even video stream. </p>
+                    <p> <strong>WebRTC</strong> <em>(Web Real-Time Communication)</em> main purpose is to provide <strong>real-time communication</strong>, it is an API that has three main services and is based on a Javascript API. It involves two different entities : <em>client</em> and <em>server</em>. The server is here to initialize the connection between the two peers before they communicate with each other directly by sharing data, audio and even video stream. </p>
 
                     <div className="card-text">
                         <p>The three services provided by WebRTC are the following ones :</p>
@@ -65,16 +69,16 @@ export class presentation extends Component {
 
                     <div class="row mb-3">
                         <div class="col-md card mr-2">
-                            <img className="img-fluid mx-auto d-block mt-4" src={InitiationPhase} alt="FrontLaunch" style={{ "width": "100%" }}></img>
+                            <a href={InitiationPhase}><img className="img-fluid mx-auto d-block mt-4" src={InitiationPhase} alt="FrontLaunch" style={{ "width": "100%" }}></img></a>
                         </div>
                         <div class="col-md card">
-                            <img className="img-fluid mx-auto d-block mt-4" src={SignallingProtocolStack} alt="FrontLaunch" style={{ "width": "50%" }}></img>
+                        <a href={SignallingProtocolStack}><img className="img-fluid mx-auto d-block mt-4" src={SignallingProtocolStack} alt="FrontLaunch" style={{ "width": "50%" }}></img></a>
                         </div>
                     </div>
 
                     <p>The first phase is called the <strong>initiation phase</strong> and it involves the two peers who want to communicate with each other and a Web Server. The web browser of both peers connects to the same server to download in their local context an application based on the WebRTC API. The server then supervises the exchanges for the connection of peer 1 and 2.  </p>
                     <p>This part are broken down into four parts :</p>
-                    <ul> 
+                    <ul>
                         <li className="mb-2"> <span className="client1">Client 1</span> send a connection request to <span className="client2">Client 2</span></li>
                         <li className="mb-2"> Web server relays the request to <span className="client2">Client 2</span> </li>
                         <li className="mb-2"> <span className="client2">Client 2</span> send a connection request to <span className="client1">Client 1</span> </li>
@@ -84,18 +88,18 @@ export class presentation extends Component {
                     <p>Web sockets are used to keep the connection open between peers. Then, through <strong>SDP </strong> <em>(Session Description Protocole)</em>, the configuration of one peer is sent to the other to allow the second peer to send data with the right configuration.</p>
                     <div class="row mb-3">
                         <div class="col-md card mr-2">
-                            <img className="img-fluid mx-auto d-block mt-3" src={PeerCommunication} alt="FrontLaunch" style={{ "width": "100%" }}></img>
+                        <a href={PeerCommunication}><img className="img-fluid mx-auto d-block mt-3" src={PeerCommunication} alt="FrontLaunch" style={{ "width": "100%" }}></img></a>
                         </div>
                         <div class="col-md card">
-                            <img className="img-fluid mx-auto d-block mt-3" src={MediaProtocolStack} alt="FrontLaunch" style={{ "width": "100%" }}></img>
+                        <a href={MediaProtocolStack}><img className="img-fluid mx-auto d-block mt-3" src={MediaProtocolStack} alt="FrontLaunch" style={{ "width": "100%" }}></img></a>
                         </div>
                     </div>
 
                     <p>Once the server requests are done and the connection is accepted, client 1 and client 2 establish a bidirectional communication where data will transit.</p>
 
 
-                    <p>Yet, in most use cases the two peers are not in the same network and are separated by NAT’s and even Firewalls. That is why WebRTC also has a set of tools that perpetuates the peer communication. Indeed, the three entities are now supported by two other servers that enable the peer to peer connection. </p> 
-                   <p> Specification of the two other servers :</p>
+                    <p>Yet, in most use cases the two peers are not in the same network and are separated by NAT’s and even Firewalls. That is why WebRTC also has a set of tools that perpetuates the peer communication. Indeed, the three entities are now supported by two other servers that enable the peer to peer connection. </p>
+                    <p> Specification of the two other servers :</p>
                     <ul>
                         <li className="mb-2"> <strong> STUN </strong> <em> (Session Traversal Utilities for NAT) </em>   : STUN servers are used to associate public addresses of peers and port associated for the communication. Thus, clients can talk directly to each other. It also allows us to bypass router restrictions.
                         </li>
@@ -135,12 +139,8 @@ export class presentation extends Component {
                                         <strong>Advantages</strong>:
 
                                         <ul>
-                                            <li>Standardized protocol using both peers to peer and server model</li>
+                                            <li>Standardized protocol using both peer to peer and server model</li>
                                             <li>Low latency for live video conference</li>
-
-                                            <li>
-                                                Standardized protocol using both peers to peer and server model
-                                            </li>
                                             <li>
                                                 Code open source
                                             </li>
@@ -163,13 +163,13 @@ export class presentation extends Component {
                                                 Uses UDP protocol, no packet loss check
                                             </li>
                                             <li>
-                                                Usage in companies can be difficult (udp and peer to peer, firewall and nat)
+                                                Usage in companies can be difficult (UDP and peer to peer, firewall and NAT)
                                             </li>
                                             <li>
                                                 No adaptive bitrate
                                             </li>
                                         </ul>
-                                
+
                                     </li>
                                 </ul>
                             </p>
@@ -183,12 +183,25 @@ export class presentation extends Component {
                                    <hr></hr>
                             </h5>
 
-                            <p className="card-text text-justify ml-3 mr-3">
-                                In just eight years of existence, WebRTC is now massively used for multiple
-                                applications. Indeed, this API that enabled people to communicate in real-time
-                                directly through web applications and namely in peer to peer mode is now used in
-                                Google Meet, Facebook Messenger and even Discord.
-                               </p>
+                            <div className="card-text text-justify ml-3 mr-3">
+                                <p>We can showcase WebRTC technology through multiple application :</p>
+                                <ul>
+                                    <li>Voice/Video Chat app </li>
+                                    <li>Live drawing app </li>
+                                    <li>Broadcast app</li>
+                                    <li>Voice/Chat translator</li>
+                                </ul>
+                                <p>Every app that needs to send and receive data in real time or at least close to real time.</p>
+                                <p>
+                                    In just eight years of existence, WebRTC is now massively used for multiple
+                                    applications. Indeed, this API that enabled people to communicate in real-time
+                                    directly through web applications and namely in peer to peer mode is now used in
+                                    <em>Google Meet, Facebook Messenger</em> and even <em>Discord</em>.
+                                </p>
+
+                            </div>
+
+
 
                         </div>
 
@@ -207,11 +220,11 @@ export class presentation extends Component {
                                 In this section, are listed links to existing WebRTC projects and applications.
                              </p>
                             <ul>
-                                <li > Official webRTC website :  <br /> <a href="https://webrtc.org">https://webrtc.org</a></li>
-                                <li> This site contains a lot of webrtc app : <br /> <a href="https://www.webrtc-experiment.com">https://www.webrtc-experiment.com</a></li>
-                                <li> A cool webRTC app that allows you to record your video stream: <br /> <a href="https://www.webrtc-experiment.com/RecordRTC/">https://www.webrtc-experiment.com/RecordRTC/</a></li>
+                                <li > Official WebRTC website :  <br /> <a href="https://webrtc.org">https://webrtc.org</a></li>
+                                <li> This site contains a lot of WebRTC app : <br /> <a href="https://www.webrtc-experiment.com">https://www.webrtc-experiment.com</a></li>
+                                <li> A cool WebRTC app that allows you to record your video stream: <br /> <a href="https://www.webrtc-experiment.com/RecordRTC/">https://www.webrtc-experiment.com/RecordRTC/</a></li>
                                 <li> WebRTC source code : <br /> <a href="https://webrtc.googlesource.com/src">https://webrtc.googlesource.com/src</a></li>
-                                
+
                             </ul>
                         </div>
                     </div>
